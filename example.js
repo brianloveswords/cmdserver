0c227b7bcb5425e73d6ef032bbe61b0238c58058
Brian J Brennan <brianloveswords@gmail.com>
2012-05-13 00:27:15 -0400

    Change argument parser

--- example.js
+++ example.js
@@ -1,45 +1,56 @@
 var util = require('util');
-var simple = require('./lib/simpleserver.js');
-var app = simple.createServer();
+var cmd = require('./lib/cmdserver.js');
+var app = cmd.createServer(process.argv);
 
 var stacks = {
   vegetables: [],
   meats: []
 };
 
-app.command('pi', function (client) {
+app.command('pi')
+  .describe('get dem numbas of pi')
+
+  .command('add <thing> <stack>')
+  .describe('add a thing to a specific stack')
+  .option('method [push|unshift]', 'method for adding. Defaults to `push`', 'push')
+
+  .command('items <stack>')
+  .describe('show the contents of a specific stack')
+
+  .command('list-stacks')
+  .describe('show all of the stacks')
+
+  .command('new <stack>')
+  .describe('make a brand new stack, hhyyeaaaaa')
+
+app.on('pi', function (client) {
   client.send(Math.PI);
 });
 
-app.command('add {thing} to {stack}( first)?', function (client, thing, stack) {
+app.on('add', function (client, thing, stack, opts) {
   if (!stacks[stack])
     return client.send(util.format('don\'t know about `%s`', stack));
-
   stacks[stack].push(thing);
   client.send(util.format('added `%s` to `%s`', thing, stack));
 });
 
-app.command('show {stack}', function (client, stack) {
+app.on('items', function (client, stack) {
   var display = util.inspect(stacks[stack], undefined, undefined, true);
   client.send(display);
 });
 
-app.command('show stacks', function (client) {
+app.on('list-stacks', function (client, stack) {
   var display = util.inspect(Object.keys(stacks), undefined, undefined, true);
   client.send(display);
 });
 
-app.command('new stack {name}', function (client, name) {
+app.on('new', function (client, name) {
   stacks[name] = [];
   client.send(util.format('adding new stack `%s`', name));
 });
 
-app.command('bye', function (client) {
-  client.send('later');
-  client.destroy();
-}).alias(['later', 'see-ya', 'peace']);
+
+app.begin();
 
 
-
-app.listen(process.argv[2] || 0);
-app.startRepl();
+//app.listen(process.argv[2] || 0);
--- test/cmdserver.test.js
+++ test/cmdserver.test.js
@@ -5,40 +5,45 @@ var should = require('should');
 describe('commanding', function () {
   var cmd = cmdserver.createServer();
 
-  cmd.command('launch <filename>')
-    .describe('launches the server like a champ')
-    .option('-p, --port [n]', 'Port to launch on', '0')
-    .option('-w, --watch', 'Watch directory for changes', false)
-    .option('-r, --restart', 'Restart server if it crashes', true)
+  cmd.command('launch <filename> [name]')
+    .describe('launches the server like a champ. Optionally give it a short name.')
+    .option('port [n]', 'Port to launch on', '0')
+    .option('watch [true|false]', 'Watch directory for changes', false)
+    .option('restart [true|false]', 'Restart server if it crashes', true)
 
 
   it('should route with short names', function (done) {
-    cmd.once('launch', function (filename, options) {
+    cmd.once('launch', function (client, filename, name, options) {
       should.exist(options.port);
       options.port.should.equal(80);
       options.restart.should.equal(false);
       filename.should.equal('testfile.js');
+      name.should.equal('test lol');
       done();
     });
-    cmd.execute('launch testfile.js -p 80 --no-restart')
+    cmd.execute('launch "testfile.js" \'test lol\' port="80" restart="false"')
   });
 
 
-  it('should route with short names', function (done) {
-    cmd.once('launch', function (filename, options) {
+  it('should should not pass in args if they do not exist', function (done) {
+    cmd.once('launch', function (client, filename, name, options) {
       should.exist(options.port);
-      options.watch.should.equal(true);
+      options.port.should.equal(999999999);
+      options.watch.should.equal(false);
       options.restart.should.equal(true);
       should.not.exist(filename);
       done();
     });
-    cmd.execute('launch -p 999999999 --watch')
+    cmd.execute('launch port = "999999999" watch = "false"')
   });
 
   it('should be able to spit out help', function () {
+    console.log(cmd.help());
+
     cmd.help().should.match(/launches the server like a champ/)
     cmd.help('launch').should.match(/launches the server like a champ/);
     cmd.help('missing').should.match(/not a recognized command/)
   });
+
 
 });
